d997ec4cefcf7e3e3c3eb6f15f9cda16a72c9946
Abdullah A. Golam <abdullahagolam@gmail.com>
2022-05-12 14:50:29 +0300

    query set

--- WEB-Django-Repo/myapp/admin.py
+++ WEB-Django-Repo/myapp/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from myapp import models
 # Register your models here.
 
+# admin.site.register(models.Student)
+# admin.site.register(models.Teacher)
+
+# admin.site.register(models.Email)
+# admin.site.register(models.Client)
+
+# admin.site.register(models.Food)
+# admin.site.register(models.Client)
+
+# admin.site.register(models.Book)
+# admin.site.register(models.Store)
+
 admin.site.register(models.Student)
--- WEB-Django-Repo/myapp/models.py
+++ WEB-Django-Repo/myapp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from datetime import datetime
+# from datetime import datetime
 
 # Create your models here.
 # دائما لما بسوي داتا بيز بروح على ملف ثاني اسمه :   settings
 # و اضيف البرنامج في جزء installed app
 # بعد ذلك اعمل migration
-class Student(models.Model):
+# class Student(models.Model):
 
-    name = models.CharField(null=True,
-                                max_length=20)
+#     name = models.CharField(null=True,
+#                                 max_length=20)
 
     # option = [
     #     ('male', 'male'),
@@ -57,6 +57,70 @@ class Student(models.Model):
 
     # agree = models.BooleanField(null=True,
     #                                 name='Srvice Agreement')
+
+    # def __str__(self):
+    #     return self.name
+
+# class Teacher(models.Model):
+#     name = models.CharField(null=True,
+#                              max_length=20)
+
+# class Email(models.Model):
+#     user_email = models.EmailField(null=True)
+
+#     def __str__(self):
+#         return self.user_email
+
+# class Client(models.Model):
+#     name = models.CharField(null= True, 
+#                                 max_length=20)
+#     email = models.OneToOneField(Email, on_delete=models.CASCADE)
+#     # PROTECT: لا تحذف ايميله
+#     # CASCADE: احذف ايميله
+
+#     def __str__(self):
+#         return self.name
+
+# class Food(models.Model):
+#     food_name = models.CharField(max_length=20, null=True)
+
+#     def __str__(self):
+#         return self.food_name
+
+# class Client(models.Model):
+#     name = models.CharField(max_length=20, null= True)
+
+#     food = models.ForeignKey(Food, on_delete=models.CASCADE, null=True)
+
+#     def __str__(self):
+#         return f'{self.name} - {self.food}'
+
+# class Book(models.Model):
+#     book_name = models.CharField(max_length=20, null=True)
+    
+#     def __str__(self):
+#         return self.book_name
+
+# class Store(models.Model):
+#     name = models.CharField(max_length=20, null= True)
+#     book = models.ManyToManyField(Book)
+
+#     def __str__(self):
+#         return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    age = models.IntegerField(null=True)
+    description = models.TextField(null=True)
+
+    class Meta:
+        # db_table = 'Student_Table'
+        # verbose_name = 'Student_Model'
+        # verbose_name_plural = 'Student'
+
+        # verbose_name = 'People'
+        # verbose_name_plural = 'People'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
